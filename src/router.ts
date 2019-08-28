03bf4c26e78e4835fa8e187e641c72fc4cdf6b28
coco-Tang <15651430850@163.com>
2019-08-29 00:05:56 +0800

    init

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -23,3 +23,4 @@ export default new Router({
     },
   ],
 });
+export default router
